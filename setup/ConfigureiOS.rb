99ca2752fab38c01dcebfcab293ac3fe3a9b7c2a
chengshaohua <tops.cheng@oyohotels.cn>
2019-08-21 18:39:02 +0800

    pod template初始化

---
+++ setup/ConfigureiOS.rb
@@ -0,0 +1,43 @@
+module Pod
+
+  class ConfigureIOS
+    attr_reader :configurator
+
+    def self.perform(options)
+      new(options).perform
+    end
+
+    def initialize(options)
+      @configurator = options.fetch(:configurator)
+    end
+
+    def perform
+
+      keep_demo = "Yes".to_sym
+
+      framework = "None".to_sym
+      configurator.set_test_framework("xctest", "m", "ios")
+
+
+      prefix = "OYO"
+
+      Pod::ProjectManipulator.new({
+        :configurator => @configurator,
+        :xcodeproj_path => "templates/ios/Example/PROJECT.xcodeproj",
+        :platform => :ios,
+        :remove_demo_project => (keep_demo == :no),
+        :prefix => prefix
+      }).run
+
+      # There has to be a single file in the Classes dir
+      # or a framework won't be created, which is now default
+      `touch Pod/Classes/ReplaceMe.m`
+
+      `mv ./templates/ios/* ./`
+
+      # remove podspec for osx
+      `rm ./NAME-osx.podspec`
+    end
+  end
+
+end
